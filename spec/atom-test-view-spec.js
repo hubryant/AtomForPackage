0fe0a75ac4cf98b0051d2662229a1dd5d014a268
胡彬 <hb6111@17u.cn>
2017-08-14 12:07:12 +0800

    package

---
+++ spec/atom-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTestView from '../lib/atom-test-view';
+
+describe('AtomTestView', () => {
+  /*it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });*/
+});
